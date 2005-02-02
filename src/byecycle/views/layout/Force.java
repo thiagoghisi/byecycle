0e296def8d64ea931b7f5667e212f201417e52e6
Rodrigo B. de Oliveira <rodrigobamboo@gmail.com>
2005-02-02 11:51:25 +0000

    klaus update

---
+++ src/byecycle/views/layout/Force.java
@@ -0,0 +1,10 @@
+//Copyright (C) 2005 Klaus Wuestefeld, Rodrigo B de Oliveira and Kent Beck
+//This is free software. It is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the license distributed along with this file for more details.
+
+package byecycle.views.layout;
+
+public interface Force {
+
+    float intensityGiven(float distance);
+
+}
