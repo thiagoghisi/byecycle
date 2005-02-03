73415061347928338a30e9f94ffd0bce7b683d3e
Rodrigo B. de Oliveira <rodrigobamboo@gmail.com>
2005-02-03 21:35:10 +0000

    unused imports

--- src/byecycle/views/layout/GraphCanvas.java
+++ src/byecycle/views/layout/GraphCanvas.java
@@ -1,10 +1,7 @@
 package byecycle.views.layout;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
 import java.util.Random;
 
