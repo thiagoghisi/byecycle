ecfa0d2d593959aef448fcb178e46d85b36be403
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2005-05-31 16:01:56 +0000

    Tests to do.

---
+++ test/TestsToDo.java
@@ -0,0 +1,9 @@
+import junit.framework.TestCase;
+
+public class TestsToDo extends TestCase {
+
+	//Do not show exceptions.
+	//Ignore self-dependencies.
+	//Do not extract nested classes as separate nodes.
+	//Show dependencies among packages.
+}
