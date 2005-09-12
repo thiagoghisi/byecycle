f56afa7de0a7363136d64ea957a04bb0f09a7929
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2005-09-12 10:45:04 +0000

    Old todo line removed.

--- byecycle/test/TestsToDo.java
+++ byecycle/test/TestsToDo.java
@@ -3,7 +3,6 @@ import junit.framework.TestCase;
 public class TestsToDo extends TestCase {
 
 	//Do not show exceptions.
-	//Ignore self-dependencies.
 	//Do not extract nested classes as separate nodes.
 	//Show dependencies among packages.
 }
