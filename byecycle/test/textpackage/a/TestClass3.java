6ddda7d4fdf889b905fadacaec4adad0ee9752d0
Dennis Cheung <hkdennis2k@gmail.com>
2005-09-17 21:53:26 +0000

    Add some example class for testing

---
+++ byecycle/test/textpackage/a/TestClass3.java
@@ -0,0 +1,9 @@
+/*
+ * Created on 2005年7月30日
+ * $id$
+ */
+package textpackage.a;
+
+public class TestClass3 extends TestClass2 implements TestInterface2{
+
+}
---
+++ byecycle/test/textpackage/a/TestInterface2.java
@@ -0,0 +1,9 @@
+/*
+ * Created on 2005年7月30日
+ * $id$
+ */
+package textpackage.a;
+
+public interface TestInterface2 extends TestInterface1{
+
+}
