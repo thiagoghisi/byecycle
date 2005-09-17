31680c07befad975f505b80eaea4c95fcb8f832d
Dennis Cheung <hkdennis2k@gmail.com>
2005-09-17 21:50:25 +0000

    ANNOTHATION and Filter by regex

--- byecycle/src/byecycle/PackageDependencyAnalysis.java
+++ byecycle/src/byecycle/PackageDependencyAnalysis.java
@@ -1,15 +1,15 @@
 //Copyright (C) 2004 Klaus Wuestefeld and Rodrigo B de Oliveira and Kent Beck.
 //This is free software. See the license distributed along with this file.
-
 package byecycle;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-
+import java.util.regex.Pattern;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.jdt.core.ICompilationUnit;
@@ -19,6 +19,7 @@ import org.eclipse.jdt.core.dom.ASTNode;
 import org.eclipse.jdt.core.dom.ASTParser;
 import org.eclipse.jdt.core.dom.ASTVisitor;
 import org.eclipse.jdt.core.dom.AbstractTypeDeclaration;
+import org.eclipse.jdt.core.dom.AnnotationTypeDeclaration;
 import org.eclipse.jdt.core.dom.ClassInstanceCreation;
 import org.eclipse.jdt.core.dom.CompilationUnit;
 import org.eclipse.jdt.core.dom.EnumDeclaration;
@@ -28,192 +29,219 @@ import org.eclipse.jdt.core.dom.ITypeBinding;
 import org.eclipse.jdt.core.dom.MethodInvocation;
 import org.eclipse.jdt.core.dom.SimpleType;
 import org.eclipse.jdt.core.dom.TypeDeclaration;
-
 import byecycle.dependencygraph.Node;
 import byecycle.preferences.PreferenceConstants;
 
 public class PackageDependencyAnalysis {
+	private final Map<String, Node<IBinding>> _nodes = new HashMap<String, Node<IBinding>>();
 
-    private final Map<String, Node<IBinding>> _nodes = new HashMap<String, Node<IBinding>>();
+	private List<String> _excludedPackages;
 
-    private List<String> _excludedPackages;
+	private List<Pattern> _excludedClassPattern;
 
     public PackageDependencyAnalysis(ICompilationUnit[] compilationUnits,
             IProgressMonitor monitor) throws JavaModelException {
 
-        if (null == monitor) {
-            monitor = new NullProgressMonitor();
-        }
+		if (null == monitor) {
+			monitor = new NullProgressMonitor();
+		}
+		
+		ASTParser parser = ASTParser.newParser(AST.JLS3);
+		
+		DependencyVisitor visitor = new DependencyVisitor();
+		
+		monitor.beginTask("dependency analysis", compilationUnits.length);
+		
+		for (ICompilationUnit each : compilationUnits) {
+			parser.setResolveBindings(true);
+			parser.setSource(each);
+			
+			monitor.subTask(each.getElementName());
+			
+			CompilationUnit node = (CompilationUnit) parser.createAST(monitor);
+			node.accept(visitor);
+			monitor.worked(1);
+			if (monitor.isCanceled()) {
+				break;
+			}
+		}
+	}
 
-        ASTParser parser = ASTParser.newParser(AST.JLS3);
+	public Collection<Node<IBinding>> dependencyGraph() {
+		return _nodes.values();
+	}
 
-        DependencyVisitor visitor = new DependencyVisitor();
+	private Node<IBinding> getNode(IBinding binding, String nodeName, JavaType kind) {
+		String key = getBindingKey(binding);
+		Node<IBinding> node = _nodes.get(key);
+		if (null == node) {
+			node = new Node<IBinding>(nodeName, kind);
+			node.payload(binding);
+			_nodes.put(key, node);
+		}
+		return node;
+	}
 
-        monitor.beginTask("dependency analysis", compilationUnits.length);
+	private String getBindingKey(IBinding binding) {
+		// return binding.getKey();
+		return binding.getJavaElement().getElementName();
+	}
 
-        for (ICompilationUnit each : compilationUnits) {
-            parser.setResolveBindings(true);
-            parser.setSource(each);
-
-            monitor.subTask(each.getElementName());
-
-            CompilationUnit node = (CompilationUnit) parser.createAST(monitor);
-            node.accept(visitor);
-
-            monitor.worked(1);
-            if (monitor.isCanceled()) {
-                break;
-            }
-        }
-    }
-
-    public Collection<Node<IBinding>> dependencyGraph() {
-        return _nodes.values();
-    }
-
-    private Node<IBinding> getNode(IBinding binding, String nodeName, JavaType kind) {
-        String key = getBindingKey(binding);
-        Node<IBinding> node = _nodes.get(key);
-        if (null == node) {
-            node = new Node<IBinding>(nodeName, kind);
-            node.payload(binding);
-            _nodes.put(key, node);
-        }
-        return node;
-    }
-
-    private String getBindingKey(IBinding binding) {
-        // return binding.getKey();
-        return binding.getJavaElement().getElementName();
-    }
-
-    private boolean ignorePackage(String packageName) {
+	private boolean ignorePackage(String packageName) {
         return "java.lang".equals(packageName)
                 || getExcludedPackages().contains(packageName);
-    }
+	}
 
-    private List<String> getExcludedPackages() {
-        if (_excludedPackages == null) {
-            _excludedPackages = Arrays.asList(ByecyclePlugin.getDefault()
-                    .getPreferenceStore().getString(
-                            PreferenceConstants.P_PACKAGE_EXCLUDES).split(
-                            "\\s+"));
-        }
-        return _excludedPackages;
-    }
+	private List<String> getExcludedPackages() {
+		if (_excludedPackages == null) {
+			_excludedPackages = Arrays.asList(ByecyclePlugin.getDefault().getPreferenceStore().getString(
+					PreferenceConstants.P_PACKAGE_EXCLUDES).split("\\s+"));
+		}
+		return _excludedPackages;
+	}
 
-    class DependencyVisitor extends ASTVisitor {
+	private List<Pattern> getClassExcludePattern() {
+		if (_excludedClassPattern == null) {
+			_excludedClassPattern = new ArrayList<Pattern>();
+			for (String str : ByecyclePlugin.getDefault().getPreferenceStore().getString(PreferenceConstants.P_PATTERN_EXCLUDES)
+					.split("\\s+")) {
+				_excludedClassPattern.add(Pattern.compile(str));
+			}
+		}
+		return _excludedClassPattern;
+	}
 
-        private Node<IBinding> _currentNode;
+	private boolean ignoreClass(String qualifiedClassName) {
+		for (Pattern pattern : getClassExcludePattern()) {
+			if (pattern.matcher(qualifiedClassName).matches()) {
+				return true;
+			}
+		}
+		return false;
+	}
 
-        private String _currentPackageName;
+	class DependencyVisitor extends ASTVisitor {
+		private Node<IBinding> _currentNode;
 
-        private boolean visit0(AbstractTypeDeclaration node) {
-            Node<IBinding> saved = _currentNode;
-            String savedPackage = _currentPackageName;
+		private String _currentPackageName;
 
-            ITypeBinding binding = node.resolveBinding();
-            _currentNode = getNode2(binding);
-            _currentPackageName = binding.getPackage().getName();
+		private boolean visit0(AbstractTypeDeclaration node) {
+			Node<IBinding> saved = _currentNode;
+			String savedPackage = _currentPackageName;
+			ITypeBinding binding = node.resolveBinding();
+			if (ignoreClass(binding.getQualifiedName()))
+				return false;
+			_currentNode = getNode2(binding);
+			_currentPackageName = binding.getPackage().getName();
+			addProvider(binding.getSuperclass());
+			for (ITypeBinding superItf : binding.getInterfaces()) {
+				addProvider(superItf);
+			}
+			visitList(node.bodyDeclarations());
+			_currentNode = saved;
+			_currentPackageName = savedPackage;
+			return false;
+			
+		}
 
-            addProvider(binding.getSuperclass());
-            for (ITypeBinding superItf : binding.getInterfaces()) {
-                addProvider(superItf);
-            }
+		@Override
+		public boolean visit(AnnotationTypeDeclaration node) {
+			return visit0(node);
+		}
 
-            visitList(node.bodyDeclarations());
+		@Override
+		public boolean visit(EnumDeclaration node) {
+			return visit0(node);
+		}
 
-            _currentNode = saved;
-            _currentPackageName = savedPackage;
-            return false;
+		@Override
+		public boolean visit(TypeDeclaration node) {
+			return visit0(node);
+		}
 
-        }
+		private Node<IBinding> getNode2(ITypeBinding binding) {
+			JavaType type = JavaType.valueOf(binding);
+			return getNode(binding, binding.getQualifiedName(), type);
+		}
 
-        public boolean visit(EnumDeclaration node) {
-            return visit0(node);
-        }
+		private void visitList(List l) {
+			for (Iterator iter = l.iterator(); iter.hasNext();) {
+				ASTNode child = (ASTNode) iter.next();
+				child.accept(this);
+			}
+		}
 
-        public boolean visit(TypeDeclaration node) {
-            return visit0(node);
-        }
+		@Override
+		public boolean visit(org.eclipse.jdt.core.dom.QualifiedType node) {
+			addProvider(node.resolveBinding());
+			return true;
+		}
 
-        private Node<IBinding> getNode2(ITypeBinding binding) {
-            JavaType type = JavaType.valueOf(binding);
-            return getNode(binding, binding.getQualifiedName(), type);
-        }
+		@Override
+		public boolean visit(SimpleType node) {
+			addProvider(node.resolveBinding());
+			return true;
+		}
 
-        private void visitList(List l) {
-            for (Iterator iter = l.iterator(); iter.hasNext();) {
-                ASTNode child = (ASTNode) iter.next();
-                child.accept(this);
-            }
-        }
+		public boolean visit(Expression node) {
+			addProvider(node.resolveTypeBinding());
+			return true;
+		}
 
-        public boolean visit(org.eclipse.jdt.core.dom.QualifiedType node) {
-            addProvider(node.resolveBinding());
-            return true;
-        }
+		@Override
+		public boolean visit(MethodInvocation node) {
+			addProvider(node.resolveTypeBinding());
+			return true;
+		}
 
-        public boolean visit(SimpleType node) {
-            addProvider(node.resolveBinding());
-            return true;
-        }
+		@Override
+		public boolean visit(ClassInstanceCreation node) {
+			addProvider(node.resolveTypeBinding());
+			return true;
+		}
 
-        public boolean visit(Expression node) {
-            addProvider(node.resolveTypeBinding());
-            return true;
-        }
+		private boolean isSelectedPackage(String packageName) {
+			return packageName.equals(_currentPackageName);
+		}
 
-        public boolean visit(MethodInvocation node) {
-            addProvider(node.resolveTypeBinding());
-            return true;
-        }
-
-        public boolean visit(ClassInstanceCreation node) {
-            addProvider(node.resolveTypeBinding());
-            return true;
-        }
-
-        private boolean isSelectedPackage(String packageName) {
-            return packageName.equals(_currentPackageName);
-        }
-
-        private void addProvider(ITypeBinding type) {
-            if (null == type)
-                return;
-            if (type.isArray())
-                type = type.getElementType();
-            if (type.isPrimitive() || type.isWildcardType())
-                return;
-            if (type.isTypeVariable()) {
-                for (ITypeBinding subType : type.getTypeBounds()) {
-                    addProvider(subType);
-                }
-                return;
-            }
-            if (type.getQualifiedName().equals(""))
-                return; // TODO: Check why this happens.
-
-            String packageName = type.getPackage().getName();
-
-            if (ignorePackage(packageName))
-                return;
-
-            if (isSelectedPackage(packageName)) {
-                if (type.isParameterizedType()) { // if Map<K,V>
-                    for (ITypeBinding subtype : type.getTypeArguments()) { // <K,V>
-                        addProvider(subtype);
-                    }
-                    final ITypeBinding erasure = type.getErasure();
-                    _currentNode.addProvider(getNode2(erasure));
-                } else {
-                    _currentNode.addProvider(getNode2(type));
-                }
-                return;
-            }
+		private void addProvider(ITypeBinding type) {
+			if (null == type)
+				return;
+			if (type.isArray())
+				type = type.getElementType();
+			if (type.isPrimitive() || type.isWildcardType())
+				return;
+			if (type.isTypeVariable()) {
+				for (ITypeBinding subType : type.getTypeBounds()) {
+					addProvider(subType);
+				}
+				return;
+			}
+			if (type.getQualifiedName().equals(""))
+				return; // TODO: Check why this happens.
+			String packageName = type.getPackage().getName();
+			if (ignorePackage(packageName))
+				return;
+			if (isSelectedPackage(packageName)) {
+				if (type.isParameterizedType()) { // if Map<K,V>
+					for (ITypeBinding subtype : type.getTypeArguments()) { // <K,V>
+						if (ignoreClass(subtype.getQualifiedName()))
+							continue;
+						addProvider(subtype);
+					}
+					final ITypeBinding erasure = type.getErasure();
+					if (ignoreClass(erasure.getQualifiedName()))
+						return;
+					_currentNode.addProvider(getNode2(erasure));
+				} else {
+					if (ignoreClass(type.getQualifiedName()))
+						return;
+					_currentNode.addProvider(getNode2(type));
+				}
+				return;
+			}
             _currentNode.addProvider(getNode(type.getPackage(), packageName,
                     JavaType.PACKAGE));
-        }
-
-    }
+		}
+	}
 }
