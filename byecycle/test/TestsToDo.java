8a856d92e5ab5d14e747fcf2ce02bd759fc47faa
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2005-09-18 03:51:43 +0000

    Test TODO removed.

--- byecycle/test/TestsToDo.java
+++ byecycle/test/TestsToDo.java
@@ -2,7 +2,6 @@ import junit.framework.TestCase;
 
 public class TestsToDo extends TestCase {
 
-	//Do not show exceptions.
 	//Do not extract nested classes as separate nodes.
 	//Show dependencies among packages.
 }
