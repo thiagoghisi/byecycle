14f1d2a2627a6e20d3d6e9df539b5ec92ea998cf
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2005-09-20 05:06:16 +0000

    Minor tweaking.

--- byecycle/src/byecycle/views/layout/algorithm/Constants.java
+++ byecycle/src/byecycle/views/layout/algorithm/Constants.java
@@ -4,8 +4,8 @@ class Constants {
 	
 	static final float AURA_THICKNESS = 20;
 
-	static final float INITIAL_IMPETUS = 20;
-	static final float MINIMUM_IMPETUS = 0.1f;
+	static final float INITIAL_IMPETUS = 10;
+	static final float MINIMUM_IMPETUS = 0.01f;
 
 	static final float NUDGE_INTENSITY = 500f;
 
