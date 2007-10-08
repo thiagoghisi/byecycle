a2c62ff2ed85f2433527abfcfb7ea29196db0b4b
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2007-10-08 10:01:48 +0000

    Playing with the algorithm

---
+++ byecycle/test/testpackage/b/TestClass1.java
@@ -0,0 +1,16 @@
+/*
+ * Created on 2005年7月30日
+ * $id$
+ */
+package textpackage.b;
+
+import java.io.File;
+import java.util.List;
+
+
+public class TestClass1 {
+	List<File> listOfFiles;
+
+
+	public void method() throws TestException1 {}
+}
---
+++ byecycle/test/testpackage/b/TestClass3.java
@@ -0,0 +1,7 @@
+/*
+ * Created on 2005年7月30日
+ * $id$
+ */
+package textpackage.b;
+
+public class TestClass3 extends TestClass2 implements TestInterface2 {}
