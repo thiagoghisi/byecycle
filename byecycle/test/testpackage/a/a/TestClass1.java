732710610ef52481643f910c1792cb8765343905
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2007-10-08 10:01:06 +0000

    Playing with the algorithm

---
+++ byecycle/test/testpackage/a/a/TestClass1.java
@@ -0,0 +1,13 @@
+/*
+ * Created on 2005年7月30日
+ * $id$
+ */
+package textpackage.a.a;
+
+import java.io.File;
+import java.util.List;
+
+
+public class TestClass1 {
+	List<File> listOfFiles;
+}
---
+++ byecycle/test/testpackage/a/a/TestClass2.java
@@ -0,0 +1,9 @@
+/*
+ * Created on 2005年7月30日
+ * $id$
+ */
+package textpackage.a.a;
+
+public class TestClass2 extends TestClass1 {
+
+}
